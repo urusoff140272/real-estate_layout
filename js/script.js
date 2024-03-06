443f836ccf46bc8a8c1ad7f5ddcf07bd55a54c97
urusoff140272 <107813230+urusoff140272@users.noreply.github.com>
2024-03-06 17:08:34 +0200

    feat: add html css and responsive sections header and about

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-"use strict"
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.body-about-block__aroow.swiper-button-next',
+        prevEl: '.body-about-block__aroow.swiper-button-prev',
+    },
+});
